7ef6628e1b56f5a5413c5aff5daf277e3561649a
RafaellaSSouza <rafaella1403@hotmail.com>
2022-01-20 23:12:52 -0300

    Creat
    Creat component whit list

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,14 @@
+<template>
+    <ol class="list-decimal bg-red-200" >
+  <li>Um</li>
+  <li>Dois</li>
+  <li>Tres </li>
+</ol>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponente'
+    }
+</script>
+

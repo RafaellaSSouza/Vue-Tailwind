9f2b554148628a1f5e6b293c13d01c52a409663b
RafaellaSSouza <rafaella1403@hotmail.com>
2022-01-23 23:30:46 -0300

    Creat
    Component and variables creat

---
+++ src/Variaveis.vue
@@ -0,0 +1,22 @@
+<template>
+    
+    <h1>{{ msg }}</h1>
+
+</template>
+
+
+<script>
+
+    var nome = 'Rafaella'
+    var sobrenome = 'Souza'
+    var variaveis = nome + sobrenome
+
+export default {
+    nome: 'Variaveis',
+    data() {
+        return {
+            msg: variaveis
+        }
+    }
+}
+</script>

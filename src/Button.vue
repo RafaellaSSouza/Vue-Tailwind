432b02760ba293d0f06eb6ef1af25a84bc4409ef
RafaellaSSouza <97993249+RafaellaSSouza@users.noreply.github.com>
2022-02-19 23:34:48 -0300

    Upload
    Adding component

---
+++ src/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="btn" :type="type">
+    <slot></slot>
+    </button>
+</template>
+<script>
+export default {
+    name: "Button",
+    props:["type"],
+
+    methods(){
+      
+    }
+}
+
+</script>
+<style lang="postcss" >
+.btn {
+  @apply 
+    h-10 
+    w-40 
+    box-border
+    rounded-md 
+    font-semibold 
+    inline-flex
+    items-center
+    justify-center
+    bg-gray-200
+}
+</style>

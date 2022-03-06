c331eb89afdd8b3f9a100e05d12008e47cc7c0f2
RafaellaSSouza <97993249+RafaellaSSouza@users.noreply.github.com>
2022-03-05 23:32:15 -0300

    Create
    component card

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <Container class="max-w-md">
+    <Box class="p-4">
+    <slot></slot>
+    </Box>
+  </Container>
+</template>
+<script>
+import Container from "./UI/Container.vue";
+import Box from "./UI/Box.vue";
+export default {
+  name: "Card",
+  components: { Container, Box },
+};
+</script>

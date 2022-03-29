64885ce7455a8f42e6895048ba474087a5c54ebd
RafaellaSSouza <97993249+RafaellaSSouza@users.noreply.github.com>
2022-03-28 23:12:44 -0300

    Create
    base button whatsapp

---
+++ src/BaseButtonWhatsapp.vue
@@ -0,0 +1,21 @@
+<template>
+  <BaseButton :variant="'outline'" type="button" @click="openWhats">
+    <IcWhatsApp />
+    Conversar no whats
+  </BaseButton>
+</template>
+<script>
+  import Vue from "vue";
+  import IcWhatsApp from "~/icons/IcWhatsApp.vue";
+  import BaseButton from "./BaseButton.vue";
+
+  export default Vue.extend({
+    name: "formContact",
+    components: { IcWhatsApp, BaseButton },
+    methods: {
+      openWhats() {
+        window.open("https://api.whatsapp.com/send?phone=55159999999999&text=Olá! Gostaria de solicitar um orçamento.");
+      },
+    },
+  });
+</script>
